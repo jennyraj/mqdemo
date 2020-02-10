54a92a6e4dbbfd580028509f7bfc874fe5284412
BEN ARULAN <barulan@meduestmail.com>
2020-02-09 23:13:21 -0500

    colors added

--- src/ui/album.tsx
+++ src/ui/album.tsx
@@ -1,23 +1,46 @@
-import React from 'react';
+import React, {useState} from 'react';
 import AppBar from '@material-ui/core/AppBar';
-import Button from '@material-ui/core/Button';
 
-
-import CssBaseline from '@material-ui/core/CssBaseline';
 import Grid from '@material-ui/core/Grid';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
-import {createStyles, fade, Theme, makeStyles} from '@material-ui/core/styles';
+import {createStyles, fade, Theme, makeStyles, createMuiTheme, MuiThemeProvider} from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
 import Link from '@material-ui/core/Link';
 import MenuIcon from '@material-ui/icons/Menu';
 import SearchIcon from '@material-ui/icons/Search';
 import {IconButton, InputBase} from "@material-ui/core";
 import Autocomplete from "./autocomplete";
-import Basic from "./basic";
-import FormDialog from "./formadd";
+
 import FormDialogAdd from "./formadd";
 import FormDialogEdit from "./formedit";
+
+{/*
+import purple from '@material-ui/colors/purple';
+import green from '@material-ui/colors/green';
+
+
+const purpleTheme = createMuiTheme({
+    palette: {
+        primary: purple,
+        secondary: green
+    }
+});
+
+const fontTheme = createMuiTheme({
+    palette: {
+        secondary: purple,
+        primary: green,
+    },
+    typography: {
+        fontFamily: ['Courier', 'Helvetica'],
+    }
+})
+
+
+const themes = [defaultTheme, purpleTheme, fontTheme]
+const    setTheme = (idx:any) => { setState( {...state, theme: themes[idx] } ) }
+*/}
 
 function Copyright() {
     return (
@@ -105,10 +128,38 @@ const useStyles = makeStyles((theme: Theme) => ({
 
 export default function Album() {
     const classes = useStyles();
+   const [state,setState]=useState();
+
+    const defaultTheme = createMuiTheme();
+    const theme = createMuiTheme({
+        palette: {
+            primary: {
+                // light: will be calculated from palette.primary.main,
+                main: '#ff4400',
+                // dark: will be calculated from palette.primary.main,
+                // contrastText: will be calculated to contrast with palette.primary.main
+            },
+            secondary: {
+                light: '#0066ff',
+                main: '#0044ff',
+                // dark: will be calculated from palette.secondary.main,
+                contrastText: '#ffcc00',
+            },
+            // Used by `getContrastText()` to maximize the contrast between
+            // the background and the text.
+            contrastThreshold: 3,
+            // Used by the functions below to shift a color's luminance by approximately
+            // two indexes within its tonal palette.
+            // E.g., shift from Red 500 to Red 300 or Red 700.
+            tonalOffset: 0.2,
+        },
+    });
 
     return (
         <React.Fragment>
-            <CssBaseline/>
+            <MuiThemeProvider
+               theme={theme}
+            >
             <AppBar position="static">
                 <Toolbar>
                     <IconButton
@@ -142,7 +193,7 @@ export default function Album() {
                 {/* Hero unit */}
                 <div className={classes.heroContent}>
                     <Container maxWidth="sm">
-                        <Typography component="h4" variant="h4" align="center"
+                        <Typography component="h6" variant="h6" align="center"
                                     color="textPrimary" gutterBottom>
                             Quick Search
                         </Typography>
@@ -172,6 +223,7 @@ export default function Album() {
                 <Copyright key={"xxx"}/>
             </footer>
             {/* End footer */}
+            </MuiThemeProvider>
         </React.Fragment>
     );
 }
